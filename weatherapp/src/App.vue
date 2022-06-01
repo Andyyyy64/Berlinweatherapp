37b624e815c3b3ec989f21f63a6c86ee916514fb
Your Name <takuyarossi77@gmail.com>
2022-06-01 21:26:30 +0900

    done

--- weatherapp/src/App.vue
+++ weatherapp/src/App.vue
@@ -1,26 +1,59 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from "axios"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      fetchedData: [
+      ],
+      startFlg: ""
+    }
+  },
+  methods: {
+    async gettemp() {
+      const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
+      const response = await axios.get(url)
+
+      const tempArr = response.data.hourly.temperature_2m
+      const timeArr = response.data.hourly.time
+      // let tempTimeArr = []
+
+      // for (let i = 0; i < tempArr.length; i++) {
+      //   tempTimeArr.push({
+      //     temp: tempArr[i],
+      //     time: timeArr[i]
+      //   })
+      // }
+      const tempTimeArr = tempArr.map((_, index) => {
+        return { 
+          temp: tempArr[index],
+          time: timeArr[index]
+        }
+      })
+
+      this.fetchedData = tempTimeArr
+      this.startFlg = true
+
+    }
   }
 }
 </script>
+<template>
+  <div class="container">
+    <div v-if="!startFlg">
+      <div class="title">
+        <h1>I know u want the temperature...press this</h1>
+      </div>
+      <button @click="gettemp" class="Btn srttemp">show temperature</button>
+    </div>
+    <div v-if="startFlg">
+      <ul v-for="(info, index) in fetchedData" :key="index">
+        <li>
+          時間:{{ info.time }}温度:{{ info.temp }}{{ info.temp >= 15 ? "熱い" : "寒い" }}
+        </li>
+      </ul>
+    </div>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  </div>
+</template>
